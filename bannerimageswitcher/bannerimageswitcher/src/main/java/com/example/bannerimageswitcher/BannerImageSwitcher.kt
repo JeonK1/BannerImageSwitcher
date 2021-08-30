5a99a64393eca76ff6a0b522a71a5cabe4652793
JeonK1 <kan5016@naver.com>
2021-08-31 01:12:09 +0900

    add example, fix library

--- bannerimageswitcher/bannerimageswitcher/src/main/java/com/example/bannerimageswitcher/BannerImageSwitcher.kt
+++ bannerimageswitcher/bannerimageswitcher/src/main/java/com/example/bannerimageswitcher/BannerImageSwitcher.kt
@@ -6,13 +6,13 @@ import android.os.Handler
 import android.os.Looper
 import android.util.AttributeSet
 import android.view.animation.AnimationUtils
+import android.widget.ImageButton
 import android.widget.ImageSwitcher
 import android.widget.ImageView
 import android.widget.RelativeLayout
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.CustomTarget
 import com.bumptech.glide.request.transition.Transition
-import kotlinx.android.synthetic.main.custom_banner_layout.view.*
 import java.lang.IllegalArgumentException
 import java.util.*
 
@@ -24,45 +24,59 @@ class BannerImageSwitcher(context: Context, attrs: AttributeSet): RelativeLayout
         var CUSTOM_NAMESPACE = "" // current project namespace (for get custom xml attribute)
         var IMAGE_SWITCH_INTERVAL = 5000L // image switch interval, default : 5초
         var page = 0 // current page
+        lateinit var timer:Timer
+
+        lateinit var banner_image:ImageSwitcher
+        lateinit var left_button:ImageButton
+        lateinit var right_button:ImageButton
     }
 
-    init {
-        // xml init
-        inflate(context, R.layout.custom_banner_layout, this)
 
+    init {
         // custom namespace init
         CUSTOM_NAMESPACE = "http://schemas.android.com/apk/res/${context.packageName}"
 
-        // interval value init
-        attrs.getAttributeValue(CUSTOM_NAMESPACE, "switch_interval")?.let{
-            IMAGE_SWITCH_INTERVAL = it.toLong()
-        }
-
-        // timer init
-        var timer = getTimer()
-        banner_image.setFactory {
-            ImageView(context).apply {
-                adjustViewBounds = true
-            }
-        }
+        // init widget
+        inflate(context, R.layout.custom_banner_layout, this) // xml init
+        banner_image = findViewById(R.id.banner_image)
+        left_button = findViewById(R.id.left_button)
+        right_button = findViewById(R.id.right_button)
 
         // left switch button clicked
         left_button.setOnClickListener {
             switchPrevPage() // switch prev image
-
-            // timer cancel, init with new instance
-            timer.cancel()
-            timer = getTimer()
+            timerReset() // timer reset
         }
 
         // right switch button clicked
         right_button.setOnClickListener {
             switchNextPage()  // switch next image
-
-            // timer cancel, init with new instance
-            timer.cancel()
-            timer = getTimer()
+            timerReset() // timer reset
         }
+
+        // timer init
+        timer = getTimer()
+        banner_image.setFactory {
+            ImageView(context).apply {
+                adjustViewBounds = true
+            }
+        }
+    }
+
+    fun setLeftButtomImageResource(resId: Int){
+        // set Left Button Image Resource
+        left_button.setImageResource(resId)
+    }
+
+    fun setRightButtomImageResource(resId: Int){
+        // set Right Button Image Resource
+        right_button.setImageResource(resId)
+    }
+
+    fun setInterval(msec:Long){
+        // set IMAGE_SWITCH_INTERVAL value
+        IMAGE_SWITCH_INTERVAL = msec
+        timerReset() // timer reset
     }
 
     fun setImageList(list:List<Any>){
@@ -150,5 +164,11 @@ class BannerImageSwitcher(context: Context, attrs: AttributeSet): RelativeLayout
                 }
             }, IMAGE_SWITCH_INTERVAL, IMAGE_SWITCH_INTERVAL)
         }
+    }
+
+    private fun timerReset(){
+        // timer cancel, init with new instance
+        timer.cancel()
+        timer = getTimer()
     }
 }
---
+++ example/BannerImageSwitcherExample/app/src/main/java/com/example/bannerimageswitcherexample/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.bannerimageswitcherexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // image list with R.drawable directories
+        val imageList1 = arrayListOf(
+            R.drawable.first_image,
+            R.drawable.second_image,
+            R.drawable.third_image
+        )
+
+        // image list with URI String (used with Glide)
+        val imageList2 = arrayListOf("https://dimg.donga.com/wps/NEWS/IMAGE/2019/10/13/97852357.2.jpg",
+            "https://i.ytimg.com/vi/jyBbnSZFLh8/hqdefault.jpg",
+            "https://lh3.googleusercontent.com/B6ZADk6grixEg3e8k-NpMwRI9-isdEwDnYbzcJhCbWQVhml20beao8NMe215Cs0Ils4zo_7rNP3aNUEBtR5BmHcRJA=w640-h400-e365-rj-sc0x00ffffff")
+
+        banner_image_view.setImageList(imageList1)
+    }
+}
